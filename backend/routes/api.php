f813a1e70a689cea4e87cbb553d720d71b022abb
isouza <isouza@gattecnologia.com.br>
2025-04-12 17:24:11 -0300

    feat: Expoe rotas de API

---
+++ backend/routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TravelOrderController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+
+    // Rotas públicas
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    // Rotas protegidas
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me', [AuthController::class, 'me']);
+    });
+});
+
+// Rotas protegidas
+Route::middleware('auth:api')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::resources([
+        'travel-orders' => TravelOrderController::class
+    ]);
+
+    Route::patch('/travel-orders/{TravelOrder}/status', [TravelOrderController::class, 'updateStatus']);
+    Route::patch('/travel-orders/{TravelOrder}/cancel', [TravelOrderController::class, 'cancelTravelOrder']);
+});
