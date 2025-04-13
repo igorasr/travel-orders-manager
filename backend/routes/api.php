753d83fcdedb05a2dbf65c323a8a522d03c18736
isouza <isouza@gattecnologia.com.br>
2025-04-13 19:22:59 -0300

    feat: Cria enpoint para recuperar usuarios

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TravelOrderController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,6 +29,7 @@ Route::middleware('auth:api')->group(function () {
     Route::resources([
         'travel-orders' => TravelOrderController::class
     ]);
+    Route::get('/users', [UserController::class, 'index']);
 
     Route::patch('/travel-orders/{TravelOrder}/status', [TravelOrderController::class, 'updateStatus']);
     Route::patch('/travel-orders/{TravelOrder}/cancel', [TravelOrderController::class, 'cancelTravelOrder']);
